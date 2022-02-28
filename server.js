a43e5df81c22cbfb9c8ace451bf8c4f66b637010
Emily <emilyadarr@gmail.com>
2022-02-28 15:52:44 -0500

    create login and signup page

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+//sets up an Express.js session and connects the session to our Sequelize Database
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SESS_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -19,6 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
